d4bfdba84a71c45507a4f11e150edf38e9cb6b35
Calvin Nisbet <cnisb044@uottawa.ca>
2015-04-24 12:12:26 -0700

    first commit

---
+++ Sunny Days/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Sunny Days
+//
+//  Created by Calvin Nisbet on 2015-04-24.
+//  Copyright (c) 2015 Calvin Nisbet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    private let apiKey = "beb199041f9a378c0e52ba94bd6ec194"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "25.705042,-80.429967", relativeToURL: baseURL)
+        let sharedSession = NSURLSession.sharedSession()
+        let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
+            println(response, error)
+        })
+        downloadTask.resume()
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
